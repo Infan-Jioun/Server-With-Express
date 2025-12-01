214ced8e3aee618eae5b38562d82d5e3c2d20550
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-12-01 10:50:03 +0600

    added server.ts with postgreSQL useing insert data

--- src/server.ts
+++ src/server.ts
@@ -51,8 +51,27 @@ initDB();
 app.get('/', (req: Request, res: Response) => {
     res.send('Next Level Express Server with TypeScript!')
 })
-app.post("/", (req: Request, res: Response) => {
-    console.log(req.body);
+app.post("/users", async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    console.log(name);
+    try {
+        const result = await pool.query(
+            `INSERT INTO users(name , email) VALUES($1, $2) RETURNING *`,
+            [name, email]
+        )
+        // console.log(result.rows[0]);
+
+        res.status(201).json({
+            success: true,
+            message: "Data insrted successfully",
+            data: result.rows[0]
+        })
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
     res.status(201).json({
         success: true, message: "Api is Working"
     })
