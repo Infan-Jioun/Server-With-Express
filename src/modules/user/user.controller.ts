e9fb69d1be8185fb20bf166455f1c7849190106f
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-12-05 00:19:12 +0600

    added bycrept use in createUser

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,9 +3,8 @@ import { userService } from "./user.service";
 import { pool } from "../../config/db";
 
 const createUser = async (req: Request, res: Response) => {
-    const { name, email } = req.body;
     try {
-        const result = await userService.createUser(name, email)
+        const result = await userService.createUser(req.body)
         res.status(200).json({
             success: true,
             message: "Successfully created user",
